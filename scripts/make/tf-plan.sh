c921646cc9325e0c32d4e0ce90c9c82c4775eb6c
ToaHartor <41634031+ToaHartor@users.noreply.github.com>
2025-07-05 02:28:41 +0200

    feat(sh): pass lb ips when planning fluxcd deployment

--- scripts/make/tf-plan.sh
+++ scripts/make/tf-plan.sh
@@ -9,11 +9,19 @@ source "scripts/utils/tf-utils.sh"
 
 tfutils::get_folder_env "$1"
 
+TF_CONFIG_VARS=()
+
 # If environment is recognized for fluxcd-$env, switch env to the right one and set TF folder and config file   
 if [ "${TF_FOLDER}" == "fluxcd" ]; then \
     pushd "terraform/${TF_FOLDER}"
     tofu workspace select "${TF_DEPLOY_ENV}"
     popd
+    # Add additional variables to terraform deployment
+    # Get ips from ../k8s/config.env.tfvars
+    traefik_ip=$(grep -Po "(?<=k8s_lb_traefik_ip\s?\=\s?\"?)[^\"\s]+" < "terraform/k8s/config.${TF_DEPLOY_ENV}.tfvars")
+    otelcol_ip=$(grep -Po "(?<=k8s_lb_otelcol_ip\s?\=\s?\"?)[^\"\s]+" < "terraform/k8s/config.${TF_DEPLOY_ENV}.tfvars")
+    TF_CONFIG_VARS+=("-var=k8s_lb_traefik_ip=${traefik_ip}")
+    TF_CONFIG_VARS+=("-var=k8s_lb_otelcol_ip=${otelcol_ip}")
     # Set git remote domain for fluxcd
     # TODO : maybe do this only for TF_DEPLOY_ENV != production
     sed -i -E "s/flux_git_remote_domain(\s+)\=(\s+)\"(.*)\"/flux_git_remote_domain\1\=\2\"$(sh scripts/get_dev_machine_ip.sh)\"/" "terraform/${TF_FOLDER}/${TF_CONFIG_VARS_FILE}"
@@ -28,5 +36,5 @@ if [ "${TF_FOLDER}" == "k8s" ]; then
         tofu plan -out "${TF_FOLDER}.tfplan" -var-file="../${TF_CONFIG_VARS_FILE}" -var-file='../../env/credentials.tfvars' 
     popd
 else
-    tofu plan -out "${TF_FOLDER}.tfplan" -var-file="${TF_CONFIG_VARS_FILE}" -var-file='../env/credentials.tfvars' 
+    tofu plan -out "${TF_FOLDER}.tfplan" -var-file="${TF_CONFIG_VARS_FILE}" -var-file='../env/credentials.tfvars' "${TF_CONFIG_VARS[@]}"
 fi
