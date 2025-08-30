4539ea8984869288f60115f6f24654c6770aa968
ToaHartor <41634031+ToaHartor@users.noreply.github.com>
2025-08-30 15:28:57 +0200

    dev: update tools script

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,17 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# Talosctl install
-curl -sL https://talos.dev/install | sh
+# Talosctl install (needs sudo to uninstall in /usr/local/bin/talosctl)
+sudo rm /usr/local/bin/talosctl
+curl -sSL https://talos.dev/install | sh
 talosctl version --client
 
 # Kubectl install
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 kubectl version --client
+
+
+# Kubectl plugin install
+## cnpg for postgresql cluster
+curl -sSfL \
+  https://github.com/cloudnative-pg/cloudnative-pg/raw/main/hack/install-cnpg-plugin.sh | \
+  sudo sh -s -- -b /usr/local/bin
 
 # dprint (formatter)
 # curl -fsSL https://dprint.dev/install.sh | sh
 
+# Flux CLI install
+curl -s https://fluxcd.io/install.sh | sudo bash
 
-# K9s (needs go installed)
-go install github.com/derailed/k9s@latest
+# Cilium CLI
+# From https://github.com/cilium/cilium-cli
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+GOOS=$(go env GOOS)
+GOARCH=$(go env GOARCH)
+curl -L --remote-name-all "https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}"
+sha256sum --check "cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum"
+sudo tar -C /usr/local/bin -xzvf "cilium-${GOOS}-${GOARCH}.tar.gz"
+rm "cilium-${GOOS}-${GOARCH}.tar.gz" "cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum"
