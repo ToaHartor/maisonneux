594f90f220f51e82269efb3902c51247c2b53c61
ToaHartor <41634031+ToaHartor@users.noreply.github.com>
2025-10-27 21:42:50 +0100

    fix(dev): use s3 url from fluxcd tfvars

--- scripts/start_devenv.sh
+++ scripts/start_devenv.sh
@@ -10,7 +10,7 @@ export REPO_PATH=$(dirname $(dirname $(realpath "$0")))
 
 source "./scripts/utils/tf-utils.sh"
 
-TARGET_TFVARS="${REPO_PATH}/terraform/k8s/config.production.tfvars"
+TARGET_TFVARS="${REPO_PATH}/terraform/fluxcd/config.production.tfvars"
 
 # Add minio credentials from production env
 # shellcheck disable=SC2155
@@ -18,10 +18,9 @@ export MINIO_ACCESS_KEY=$(tfutils::get_tfvars_quoted_key "minio_access_key" "${T
 # shellcheck disable=SC2155
 export MINIO_SECRET_KEY=$(tfutils::get_tfvars_quoted_key "minio_secret_key" "${TARGET_TFVARS}")
 
-TRUENAS_HOST=$(tfutils::get_tfvars_quoted_key "truenas_vm_host" "${TARGET_TFVARS}")
-MINIO_PORT=$(tfutils::get_tfvars_number_key "minio_port" "${TARGET_TFVARS}")
+MINIO_ADDRESS=$(tfutils::get_tfvars_quoted_key "minio_access_url" "${TARGET_TFVARS}")
 
 # Set zot remote storage to the minio bucket in truenas
-jq ".storage.storageDriver.regionendpoint |= \"http://${TRUENAS_HOST}:${MINIO_PORT}\"" "${REPO_PATH}/dev/zot-config.json" > "${REPO_PATH}/tmp/zot-config.json"
+jq ".storage.storageDriver.regionendpoint |= \"http://${MINIO_ADDRESS}\"" "${REPO_PATH}/dev/zot-config.json" > "${REPO_PATH}/tmp/zot-config.json"
 
 podman compose -f "$SCRIPTPATH/../dev/docker-compose.yaml" up -d
