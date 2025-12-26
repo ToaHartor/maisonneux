966057c51e9142f707beee72326411403aa3a98f
ToaHartor <41634031+ToaHartor@users.noreply.github.com>
2025-12-26 02:38:10 +0100

    fix(tf): add namespace annotations

--- terraform/fluxcd/namespaces.tf
+++ terraform/fluxcd/namespaces.tf
@@ -1,11 +1,18 @@
 resource "kubernetes_namespace_v1" "flux_system" {
   metadata {
     name = "flux-system"
+    annotations = {
+      "kustomize.toolkit.fluxcd.io/prune" = "disabled"
+    }
   }
 }
 
 resource "kubernetes_namespace_v1" "external_secrets" {
   metadata {
     name = "external-secrets"
+    annotations = {
+      "kustomize.toolkit.fluxcd.io/prune" = "disabled"
+    }
+
   }
 }
